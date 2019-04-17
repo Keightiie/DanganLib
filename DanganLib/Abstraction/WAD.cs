efd9566dd29572390fd9a3164f17dd4836162214
Ziella Rose <ziellatheneko@gmail.com>
2019-04-18 02:15:57 +1000

    More wad variables.

--- DanganLib/Abstraction/WAD.cs
+++ DanganLib/Abstraction/WAD.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DanganLib.Abstraction
 {
     public class WAD
     {
         string Magic { get; set; } = "AGAR";
-
+        public int MajorVersion { get; private set; } = 1;
+        public int MinorVersion { get; private set; } = 1;
+        byte[] Header { get; set; }
         public int FileCount { get; private set; }
         public int DirCount { get; private set; }
-        public string filePath { get; set; }
-        int MajorVersion { get; set; }
-        int MinorVersion { get; set; }
+
+
+        FileStream file;
+
+
+
+        void ParseFileInfo(BinaryReader wadBR)
+        {
+
+        }
+
+
 
         ///<summary>
         ///Exports the contents of a wad file to a folder.
@@ -25,13 +37,22 @@ namespace DanganLib.Abstraction
         }
 
         ///<summary>
-        ///Compiles the contents of a folder into a wad file.
+        ///Compiles everyting into into a wad file.
         ///</summary>
         public void Compile(string compileDir, string exportPath)
         {
 
         }
-
+        
 
     }
+
+    class FileEntry
+    {
+        string FileName { get; set; }
+        long FileSize { get; set; }
+        long FileOffset { get; set; }
+        bool External { get; set; }
+    }
+
 }
