a96af5724e7e1c24762b5fcbfb133f74ba0911a8
Ziella Rose <ziellatheneko@gmail.com>
2019-04-18 02:24:09 +1000

    Start of WAD parsing.

--- DanganLib/Abstraction/WAD.cs
+++ DanganLib/Abstraction/WAD.cs
@@ -12,18 +12,34 @@ namespace DanganLib.Abstraction
         string Magic { get; set; } = "AGAR";
         public int MajorVersion { get; private set; } = 1;
         public int MinorVersion { get; private set; } = 1;
+        public int HeaderSize { get; set; } = 0;
         byte[] Header { get; set; }
-        public int FileCount { get; private set; }
+        public int FileCount { get; private set; } = 0;
+        List<FileEntry> files = new List<FileEntry>();
         public int DirCount { get; private set; }
-
 
         FileStream file;
 
 
 
-        void ParseFileInfo(BinaryReader wadBR)
-        {
+        public WAD() { }
 
+        public WAD(FileStream fs) {
+            file = fs;
+            BinaryReader wadBR = new BinaryReader(file);
+            ParseFile(wadBR);
+
+        }
+
+
+        void ParseFile(BinaryReader wadBR)
+        {
+            Magic = Encoding.UTF8.GetString(wadBR.ReadBytes(4));
+            MajorVersion = wadBR.ReadInt32();
+            MinorVersion = wadBR.ReadInt32();
+            HeaderSize = wadBR.ReadInt32();
+            Header = wadBR.ReadBytes(HeaderSize);
+            FileCount = wadBR.ReadInt32();
         }
 
 
@@ -53,6 +69,7 @@ namespace DanganLib.Abstraction
         long FileSize { get; set; }
         long FileOffset { get; set; }
         bool External { get; set; }
+        string source { get; set; }
     }
 
 }
