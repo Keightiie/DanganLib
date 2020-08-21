4f009bb2e97b1108b69749440df77c64b09dd947
Ziiella <35301742+Ziiella@users.noreply.github.com>
2020-08-21 11:01:30 +1000

    Renamed OBB to TB since the archive isn't used only for the OBB files.

--- DanganLib/Dangan/Anniversary/TB.cs
+++ DanganLib/Dangan/Anniversary/TB.cs
@@ -7,10 +7,10 @@ using System.CodeDom;
 
 namespace DanganLib.Dangan.Anniversary
 {
-    public class OBB
+    public class TB
     {
-        BinaryReader OBBFile;
-        BinaryWriter OBBFileWriter;
+        BinaryReader TBFile;
+        BinaryWriter TBFileWriter;
 
 
         public string Signature { get; private set; }
@@ -20,29 +20,29 @@ namespace DanganLib.Dangan.Anniversary
 
         public List<Archive.FileEntry> FileEntries = new List<Archive.FileEntry>();
 
-        public OBB()
+        public TB()
         {
 
         }
 
         void Parse(BinaryReader BinaryFile)
         {
-            OBBFile = BinaryFile ?? throw new ArgumentException("Alreading operating on a file.", "br");
+            TBFile = BinaryFile ?? throw new ArgumentException("Alreading operating on a file.", "br");
 
-            Signature = Encoding.UTF8.GetString(OBBFile.ReadBytes(2));
+            Signature = Encoding.UTF8.GetString(TBFile.ReadBytes(2));
 
             if (!ValidSignature()) return;
 
-            OBBFile.ReadBytes(5); //Unknown bytes
-            FileCount = OBBFile.ReadUInt32();
+            TBFile.ReadBytes(5); //Unknown bytes
+            FileCount = TBFile.ReadUInt32();
 
             for (int i = 0; i < FileCount; i++)
             {
                 Archive.FileEntry FileEntry = new Archive.FileEntry();
-                FileEntry.Name = OBBFile.ReadString();
-                FileEntry.Offset = OBBFile.ReadUInt32();
-                FileEntry.Size = OBBFile.ReadUInt32();
-                OBBFile.ReadBytes(16); //Unknown bytes
+                FileEntry.Name = TBFile.ReadString();
+                FileEntry.Offset = TBFile.ReadUInt32();
+                FileEntry.Size = TBFile.ReadUInt32();
+                TBFile.ReadBytes(16); //Unknown bytes
 
                 FileEntries.Add(FileEntry);
             }
@@ -52,16 +52,16 @@ namespace DanganLib.Dangan.Anniversary
 
         public void Export(string exportPath)
         {
-            if (OBBFile == null)
+            if (TBFile == null)
                 throw new InvalidDataException("There is no file loaded to extract from.");
 
             for (int i = 0; i < FileEntries.Count; i++)
             {
                 Directory.CreateDirectory($"{exportPath}/{Path.GetDirectoryName(FileEntries[i].Name)}");
-                OBBFile.BaseStream.Position = FileEntries[i].Offset;
+                TBFile.BaseStream.Position = FileEntries[i].Offset;
                 var NewFile = File.Create($"{exportPath}/{FileEntries[i].Name}");
                 BinaryWriter FileWriter = new BinaryWriter(NewFile);
-                FileWriter.Write(OBBFile.ReadBytes((int)FileEntries[i].Size));
+                FileWriter.Write(TBFile.ReadBytes((int)FileEntries[i].Size));
                 FileWriter.Close();
             }
 
@@ -70,55 +70,55 @@ namespace DanganLib.Dangan.Anniversary
         public void Pack(string ImportPath)
         {
 
-            if (OBBFile != null) throw new ArgumentException("A file is already being opperated on.", "br");
+            if (TBFile != null) throw new ArgumentException("A file is already being opperated on.", "br");
 
             uint offsetLocation = 0;
             uint FileEntriesEnd = 0;
 
-            OBBFileWriter = new BinaryWriter(new FileStream($"{ImportPath}_packed.obb", FileMode.Create));
-            OBBFileWriter.Write(Encoding.UTF8.GetBytes("TP"));
-            OBBFileWriter.Write((byte)1);
+            TBFileWriter = new BinaryWriter(new FileStream($"{ImportPath}_packed.obb", FileMode.Create));
+            TBFileWriter.Write(Encoding.UTF8.GetBytes("TP"));
+            TBFileWriter.Write((byte)1);
 
             for (int i = 0; i < 4; i++) 
             {
-                OBBFileWriter.Write((byte)0);
+                TBFileWriter.Write((byte)0);
             }
 
 
             string[] allfiles = Directory.GetFiles($"{ImportPath}\\", "*.*", SearchOption.AllDirectories);
-            OBBFileWriter.Write((uint)allfiles.Count());
+            TBFileWriter.Write((uint)allfiles.Count());
 
             foreach (var file in allfiles)
             {
                 FileInfo info = new FileInfo(file);
                 string str = info.FullName.Remove(0, ImportPath.Length + 1);
-                OBBFileWriter.Write(str);
-                OBBFileWriter.Write(offsetLocation);
-                OBBFileWriter.Write((uint)info.Length);
+                TBFileWriter.Write(str);
+                TBFileWriter.Write(offsetLocation);
+                TBFileWriter.Write((uint)info.Length);
                 for (int i = 0; i < 16; i++)
                 {
-                    OBBFileWriter.Write((byte)0);
+                    TBFileWriter.Write((byte)0);
                 }
 
             }
 
-            FileEntriesEnd = (uint)OBBFileWriter.BaseStream.Position;
-            OBBFileWriter.BaseStream.Position = 3;
-            OBBFileWriter.Write(FileEntriesEnd - 7);
+            FileEntriesEnd = (uint)TBFileWriter.BaseStream.Position;
+            TBFileWriter.BaseStream.Position = 3;
+            TBFileWriter.Write(FileEntriesEnd - 7);
 
-            OBBFileWriter.BaseStream.Position = 11;
+            TBFileWriter.BaseStream.Position = 11;
             foreach (var file in allfiles)
             {
                 FileInfo info = new FileInfo(file);
                 string str = info.FullName.Remove(0, ImportPath.Length + 1);
                 str = str.Replace("\\", "/");
-                OBBFileWriter.Write(str);
-                OBBFileWriter.Write(FileEntriesEnd);
+                TBFileWriter.Write(str);
+                TBFileWriter.Write(FileEntriesEnd);
                 FileEntriesEnd += (uint)info.Length;
-                OBBFileWriter.Write((uint)info.Length);
+                TBFileWriter.Write((uint)info.Length);
                 for (int i = 0; i < 16; i++)
                 {
-                    OBBFileWriter.Write((byte)0);
+                    TBFileWriter.Write((byte)0);
                 }
 
             }
@@ -127,19 +127,19 @@ namespace DanganLib.Dangan.Anniversary
             {
                 FileInfo info = new FileInfo(file);
                 BinaryReader ImportFile = new BinaryReader(new FileStream(info.FullName, FileMode.Open));
-                OBBFileWriter.Write(ImportFile.ReadBytes((int)ImportFile.BaseStream.Length));
+                TBFileWriter.Write(ImportFile.ReadBytes((int)ImportFile.BaseStream.Length));
                 ImportFile.Close();
 
             }
-            OBBFileWriter.Close();
+            TBFileWriter.Close();
 
 
-            OBBFile = new BinaryReader(new FileStream($"{ImportPath}_packed.obb", FileMode.Open));
+            TBFile = new BinaryReader(new FileStream($"{ImportPath}_packed.obb", FileMode.Open));
         }
 
         public void Close() 
         {
-            if (OBBFile == null) throw new ArgumentException("No file to close.", "br");
+            if (TBFile == null) throw new ArgumentException("No file to close.", "br");
         }
 
     }
